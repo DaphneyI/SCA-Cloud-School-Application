1e0325570f0439e45e89d6856a87a4c0f8cff417
Daphney Igwe <daphneyigwe@gmail.com>
2021-07-19 16:42:14 +0100

    updated docker container

--- docker/src/app.py
+++ docker/src/app.py
@@ -6,4 +6,6 @@ app = Flask(__name__)
 # define routes
 @app.route('/')
 def home():
-    return """<h1 style='text-align:center; text-transform:capitalize'>Welcome to SCA Cloud School Application</h1>"""
+    return """<h1 style='text-align:center; line-height:1.5; padding-top:150px'>
+    Welcome to SCA Cloud School Application , <br> this is my first assessment
+    </h1>"""
